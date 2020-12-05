0f820debbac9e79a0c4ca8fbd7a305159e677664
theRealAyan <ayanispro420@gmail.com>
2020-12-06 01:38:13 +0600

    Added some stylings

--- src/index.js
+++ src/index.js
@@ -9,35 +9,44 @@ const fetch = require('node-fetch')
 const { prefix, token } = require('./config.json')
 const server = require('./server')
 let time
-let res_time, res_start, res_end
+
 setInterval(() => {
-	time = c.gray(moment().format('HH:mm:ss:SSS '))
+    time = c.gray(moment().format('HH:mm:ss:SSS '))
 })
 
+console.log(c.blue(`
+┏━
+┃	┏┳┓╻╺┳╸┏━┓╻ ╻╻ ╻┏━┓   ┏━┓┏━┓┏━┓ ┏┓┏━╸┏━╸╺┳╸
+	┃┃┃┃ ┃ ┗━┓┃ ┃┣━┫┣━┫╺━╸┣━┛┣┳┛┃ ┃  ┃┣╸ ┃   ┃
+	╹ ╹╹ ╹ ┗━┛┗━┛╹ ╹╹ ╹   ╹  ╹┗╸┗━┛┗━┛┗━╸┗━╸ ╹  ┃
+ 						   ━┛
+`))
 client.commands = new Discord.Collection()
 client.events = new Discord.Collection()
 
 fs.readdir('./commands/', (err, files) => {
     if (err) return console.log(err)
+    console.log(time + c.bgCyanBright('loading commands ...'))
     files.forEach(file => {
         if (!file.endsWith(".js")) return;
         let props = require(`./commands/${file}`)
-        console.log(time + 'loaded ' + c.bgBlue('commands/' + file))
+        console.log(time + c.bgBlue('loaded ' + 'commands/' + file))
         let commandName = file.split(".")[0]
         client.commands.set(commandName, props)
     })
 })
 fs.readdir('./events/', (err, files) => {
     if (err) console.log(err)
-        files.forEach(file => {
-            let eventFunc = require(`./events/${file}`)
-            console.log(time + 'loaded ' + c.bgBlue('events/' + file))
-            let eventName = file.split(".")[0]
-            client.on(eventName, (...args) => eventFunc.run(client, ...args))
+    console.log(time + c.bgCyanBright('loading events ...'))
+    files.forEach(file => {
+    	let eventFunc = require(`./events/${file}`)
+        console.log(time + c.bgBlue('loaded ' + 'events/' + file))
+		let eventName = file.split(".")[0]
+        client.on(eventName, (...args) => eventFunc.run(client, ...args))
     })
 })
-
 client.on('ready', () => {
+	console.log(time + c.bgGreen('starting ...'))
 	server.run()
 	console.log(time + 'server started, port: ' + server.port)
 	console.log(time + 'logged in as ' + client.user.tag)
