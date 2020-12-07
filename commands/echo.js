906d796fc65a135e9da63c95ecb8954557d6b03b
theRealAyan <ayanispro420@gmail.com>
2020-12-07 08:26:38 +0600

    Add echo command

---
+++ commands/echo.js
@@ -0,0 +1,8 @@
+const { owner_id } = require('../config.json')
+exports.run = (client, message, args) => {
+	if(message.author.id != owner_id) return
+	args = args.join(' ')
+	message.channel.send(args)
+	message.delete()
+}
+exports.help = ''
