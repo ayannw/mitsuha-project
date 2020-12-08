2ebc47228a8a0c88093c34fcd4d023f3586fd744
theRealAyan <ayanispro420@gmail.com>
2020-12-08 13:06:56 +0600

    Add avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+const rhex = require('random-hex-color')
+let user, embed
+embed = new Discord.MessageEmbed()
+			.setColor('#' + rhex())
+			
+exports.run = (client, message, args) => {
+	user = message.author
+	if(message.mentions.users.size > 0){
+		user = message.mentions.users.first()
+	}
+	embed = new Discord.MessageEmbed()
+			.setTitle(user.tag)
+			.setImage(user.avatarURL({ dynamic: true, size: 2048 }))
+			.setColor(rhex())
+			.setTimestamp()
+	message.channel.send(embed)
+}
+exports.help = 'Display someone\'s avatar.'
--- pu.sh
+++ pu.sh
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "Updated some commands"
+git commit -m "Add avatar command"
 git push
