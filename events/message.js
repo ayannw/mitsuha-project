c83100c5a0c650961a0e2c6801e4f59269259d8c
theRealAyan <ayanispro420@gmail.com>
2020-12-21 22:28:44 +0600

    add error handler for message event

--- events/message.js
+++ events/message.js
@@ -1,18 +1,22 @@
 const { prefix } = require('../config.json')
-const { error } = require('../logger')
+const { error, warn } = require('../logger')
 
 exports.run = async(client, message) => {
 	if (message.author.bot) return;
 
-	main = String(message.content.split(' ')[0]).toLowerCase()
-	cmd = main.split(prefix)[1]
-	args = message.content.split(main)[1]
-	commandfile = client.commands.get(cmd) 
-    			|| client.aliases.get(cmd)
-	if(!commandfile) return
 	try {
-		commandfile.run(client, message, args)
-	} catch(err){
-		error(err)
+		main = String(message.content.split(' ')[0]).toLowerCase()
+		cmd = main.split(prefix)[1]
+		args = message.content.split(main)[1]
+		commandfile = client.commands.get(cmd) 
+    			|| client.aliases.get(cmd)
+		if(!commandfile) return
+		try {
+			commandfile.run(client, message, args)
+		} catch(err){
+			error(err)
+		}
+	} catch {
+		warn('new message does not start with prefix')
 	}
 }
