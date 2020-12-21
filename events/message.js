bba2eec5c788eb6acadc2fef723669d45d27506c
theRealAyan <ayanispro420@gmail.com>
2020-12-21 22:40:47 +0600

    idk

--- events/message.js
+++ events/message.js
@@ -2,14 +2,12 @@ const { prefix } = require('../config.json')
 const { error, warn } = require('../logger')
 
 exports.run = async(client, message) => {
-	if (message.author.bot) return;
-
+	if (message.author.bot) return
 	try {
 		main = String(message.content.split(' ')[0]).toLowerCase()
 		cmd = main.split(prefix)[1]
 		args = message.content.split(main)[1]
-		commandfile = client.commands.get(cmd) 
-    			|| client.aliases.get(cmd)
+		commandfile = client.commands.get(cmd)
 		if(!commandfile) return
 		try {
 			commandfile.run(client, message, args)
