92f5c041146e3ed683e7a66ed3352cc57d0e695a
Ayan <ayanxdev@gmail.com>
2021-01-17 12:59:06 +0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
   name : 'ping',
   run : (client, message, args) => {
     message.channel.send('?').then(m => {
-      ping = m.createdTimestamp -  message.createdTimestamp
+      const ping = m.createdTimestamp -  message.createdTimestamp
       return m.edit('<:online:735517961602727957> Pong! Latency ' + ping + 'ms, heartbeat ' + client.ws.ping + 'ms <a:heartbeat:785064044322488330>')
     })
   },
