5bde09d4947ad24a1d1f371e5284fefffde1c5c8
Ayan <ayanxdev@gmail.com>
2021-01-19 18:34:57 +0600

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -1,7 +1,7 @@
 module.exports = {
   name : 'support',
   run : (client, message, args) => {
-    message.channel.send('**GitHub repo**: \n<https://github.com/theRealAyan/mitsuha-project>\nJoin support server**: <https://discord.gg/6Pwak89TTg>')
+    message.channel.send('**GitHub repo**: \n<https://github.com/theRealAyan/mitsuha-project>\n**Join support server**: <https://discord.gg/6Pwak89TTg>')
   },
   help : ''
 }
