76ba06ead651d8d8a0c13054c9d0e4143780b8da
theRealAyan <ayanispro420@gmail.com>
2021-01-24 00:21:19 +0600

    updated command handler

--- src/handlers/command.ts
+++ src/handlers/command.ts
@@ -3,6 +3,8 @@ import * as logger from '@ayanthedev/colorlogs';
 import { Collection } from 'discord.js';
 import { green } from 'colorette';
 
+const s: number = new Date().getTime();
+
 let cmds: Collection<string, any> = new Collection();
 let path: string = process.cwd() + '/build/commands/';
 
@@ -11,10 +13,16 @@ fs.readdir(path, (err, files): void => {
     logger.warn('loading commands ...');
     files.forEach(file => {
         let props = require(`../commands/${file}`);
-        logger.success('loaded ' + 'command: ' + green(file));
+        let n: number = new Date().getTime();
+        logger.success('├─ loaded ' 
+        	+ green(file)
+        	+ ' in '
+        	+ String(n-s)
+        	+ 'ms');
         let commandName = file.split(".")[0];
         commands.set(commandName, props);
     });
+    logger.success(`└─ loaded ${cmds.size} commands`);
 });
 
 export const commands = cmds;
