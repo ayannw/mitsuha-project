8236ff498f198c3db4e95b0815675928d8550203
theRealAyan <ayanispro420@gmail.com>
2021-01-28 12:48:56 +0600

    Update tool: getMember.ts

--- src/tools/getMember.ts
+++ src/tools/getMember.ts
@@ -1,15 +1,23 @@
-import { Message } from 'discord.js'
+import { Message } from 'discord.js';
 
-export function getMember(message: Message): any {
+export function getMember(message: Message, args: string[]): any {
   let member: any;
-  if(message.mentions.users.size == 0) {
-    member = message.guild.members.cache.get(message.author.id);
-  } else if(message.mentions.users.size == 1) {
-    member = message.mentions.users.first();
-  } else if(message.mentions.users.size > 1) {
-    member = false;
+  let guild = message.guild;
+	
+  if(args.length == 0) {
+  	return member = guild.members.cache.get(message.author.id);
   } else {
-    member = false;
+  	member = message.mentions.users.first()
+  		|| guild.members.cache.get(args[0])
+  		|| guild.members.cache.find(
+  		(m) => m.user.username == args[0]
+  			|| m.user.username.startsWith(args[0])
+			|| m.displayName == args[0]
+			|| m.displayName.startsWith(args[0])
+			|| m.nickname == args[0]
+			|| m.user.tag == args[0]
+		)
+		|| false;
   };
 
   return member;
